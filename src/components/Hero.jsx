5f4ca11873e939a142bfe7e9362a451ee29ce76d
rizkyhalzah <rizkyhalzah154@gmail.com>
2025-10-07 22:04:13 +0700

    fix cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,27 +10,32 @@ function App() {
             <p className="text-lg text-gray-300 mb-6">
               Hello everyone, nice to meet you. I have an interest in
               programming, especially in{" "}
-              <span className="text-purple-500">Frontend & Game Developer</span>{""} using
-              modern technologies and creating engaging games.
+              <span className="text-purple-500">Frontend & Game Developer</span>
+              {""} using modern technologies and creating engaging games.
             </p>
             <div className="flex gap-4 justify-center md:justify-start">
               <a
-                href="/assets/Cv.Iaan.pdf"
+                href="/Cv.Iaan.pdf"
                 download
-                className="px-6 py-3 bg-purple-500 rounded-lg hover:bg-purple-600 transition">
+                className="px-6 py-3 bg-purple-500 rounded-lg hover:bg-purple-600 transition"
+              >
                 Download CV
               </a>
               <a
                 href="#contact"
-                className="px-6 py-3 border border-purple-500 rounded-lg hover:bg-purple-500 hover:text-white transition">
+                className="px-6 py-3 border border-purple-500 rounded-lg hover:bg-purple-500 hover:text-white transition"
+              >
                 Contact Me
               </a>
             </div>
           </div>
           {/* photo profile */}
           <div className="flex-1 flex justify-center mt-8 md:mt-0">
-            <img src="/assets/proyek/orang.jpg" alt="profile"
-            className="w-64 h-64 rounded-full border-4 border-purple-500 shadow-lg object-cover object-top"/>
+            <img
+              src="/assets/proyek/orang.jpg"
+              alt="profile"
+              className="w-64 h-64 rounded-full border-4 border-purple-500 shadow-lg object-cover object-top"
+            />
           </div>
         </div>
       </section>
